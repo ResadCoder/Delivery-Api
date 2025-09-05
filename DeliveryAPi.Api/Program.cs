a92b40cab65b689f0c2556d2da4a58a295910096
ResadCoder <resadsadiqov62@gamil.com>
2025-09-05 20:08:52 +0400

    swagger check removed

--- DeliveryAPi.Api/Program.cs
+++ DeliveryAPi.Api/Program.cs
@@ -49,11 +49,9 @@ builder.Services.AddApplication();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+
 
 app.UseHttpsRedirection();
 app.UseAuthentication();
